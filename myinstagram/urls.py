105a17998194501cdaa6521c73a86188da30f2fe
johnstat101 <johnjorogek@gmail.com>
2022-03-06 21:33:04 +0300

    add base and index templates

--- myinstagram/urls.py
+++ myinstagram/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from instagram.views import PostLikeToggle, PostLikeAPIToggle
+from myinstagram.views import PostLikeToggle, PostLikeAPIToggle
 from . import views
 
 urlpatterns = [
