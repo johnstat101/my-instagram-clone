f66265363b84c5fd498e5c21ef48c0cc9f8e3378
johnstat101 <johnjorogek@gmail.com>
2022-03-08 18:13:39 +0300

    update requirements file

--- myinstagram/urls.py
+++ myinstagram/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from myinstagram.views import PostLikeToggle, PostLikeAPIToggle
+from myinstagram.views import PostLikeToggle
 from . import views
 
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     path('user_profile/<username>/', views.user_profile, name='user_profile'),
     path('post/<id>', views.post_comment, name='comment'),
     path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
-    path('api/post/<id>/like', PostLikeAPIToggle.as_view(), name='liked-api'),
     path('like', views.like_post, name='like_post'),
     path('search/', views.search_profile, name='search'),
     path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
--- myinstagram/views.py
+++ myinstagram/views.py
@@ -7,9 +7,6 @@ from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.views.generic import RedirectView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
 
 
 def signup(request):
@@ -132,34 +129,6 @@ class PostLikeToggle(RedirectView):
         else:
             obj.likes.add(user)
         return url_
-
-
-class PostLikeAPIToggle(APIView):
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, id=None, format=None):
-        # id = self.kwargs.get('id')
-        obj = get_object_or_404(Post, pk=id)
-        url_ = obj.get_absolute_url()
-        user = self.request.user
-        updated = False
-        liked = False
-        if user in obj.likes.all():
-            liked = False
-            obj.likes.remove(user)
-        else:
-            liked = True
-            obj.likes.add(user)
-        updated = True
-        data = {
-
-            'updated': updated,
-            'liked': liked,
-        }
-        print(data)
-        return Response(data)
-
 
 def like_post(request):
     # image = get_object_or_404(Post, id=request.POST.get('image_id'))
